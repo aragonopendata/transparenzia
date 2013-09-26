b4024cf34f08baf2c20c94a93e3c8bc89ef9a11c
pablojimeno <pablojimeno@sonicbyte.com>
2013-09-26 18:38:14 +0200

    Add rails with postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Transparenzia::Application.config.secret_key_base = '0d5f4399a5a233e351c80f42dccbe98e92fa53d25230d4660b523a14acc294d81e8f4ada51567a76206fd1e6779e7230455517c5927e5deadcdd4d7ff9d55052'
