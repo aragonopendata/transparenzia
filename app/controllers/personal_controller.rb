6497681166fd9e8770f129fee7e5b8b198dc22fa
pablojimeno <pablojimeno@sonicbyte.com>
2013-11-19 14:26:24 +0100

    Add departments select box, and fix variables in spanish

--- app/controllers/personal_controller.rb
+++ app/controllers/personal_controller.rb
@@ -22,7 +22,7 @@ class PersonalController < ApplicationController
 
     @titulations = @people.collect{|person| [person['GR_Titulac'], person['Desc_GR_titulac']]}.uniq
     @modes = @people.collect{|person| [person['Modalidad'], person['Desc Modalidad']]}.uniq
-    @divisions = @people.collect{|person| [person['DivP'], person['División de personal']]}.uniq
+    @divisions = @people.collect{|person| [person['División de personal'], person['División de personal']]}.uniq
     @subdivisions = @people.collect{|person| [person['SubPer'], person['Subdivisión de personal']]}.uniq
     @groups = @people.collect{|person| [person['GrPer'], person['Grupo de personal']]}.uniq
     @areas = @people.collect{|person| [person['ÁPers'], person['Área de personal']]}.uniq
