c750afefb180e05d9def4cdcbdb9fe4b4ae875bc
danilat <danilat83@gmail.com>
2013-11-19 12:19:37 +0100

    implemented helpers for provinces and districts selectors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,56 @@
 module ApplicationHelper
+  PROVINCES = ["Huesca", "Teruel", "Zaragoza"]
+  DISTRICTS = {
+    "Huesca" => [
+      "Alto Gállego",
+      "Bajo Cinca / Baix Cinca",
+      "Cinca Medio",
+      "Hoya de Huesca / Plana de Uesca",
+      "La Jacetania",
+      "La Litera / La Llitera",
+      "La Ribagorza",
+      "Los Monegros",
+      "Sobrarbe",
+      "Somontano de Barbastro"
+    ],
+    "Teruel" => [
+      "Andorra-Sierra de Arcos",
+      "Bajo Aragón",
+      "Bajo Martín",
+      "Cuencas Mineras",
+      "Comunidad de Teruel",
+      "Gúdar-Javalambre",
+      "Jiloca",
+      "Maestrazgo",
+      "Matarraña / Matarranya",
+      "Sierra de Albarracín",
+    ],
+    "Zaragoza" => [
+      "Aranda",    
+      "Bajo Aragón-Caspe / Baix Aragó-Casp",
+      "Campo de Belchite",
+      "Campo de Borja",
+      "Campo de Cariñena",
+      "Campo de Daroca",
+      "Cinco Villas",
+      "Comunidad de Calatayud",
+      "DC Zaragoza",
+      "Ribera Alta del Ebro",
+      "Ribera Baja del Ebro",
+      "Tarazona y el Moncayo",
+      "Valdejalón"
+    ]
+  }
+  def districts(province=nil)
+    if province
+      distr = DISTRICTS[province]
+    else
+      distr = DISTRICTS.collect{|k, v| v}.flatten
+    end
+    distr.insert(0, "seleccione una comarca")
+  end
+
+  def provinces
+    PROVINCES.insert(0, "seleccione provincia")
+  end
 end
