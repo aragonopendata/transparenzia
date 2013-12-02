0c4d59f1765adb7dcf7a38c1c3ce8a046bd37865
pablojimeno <pablojimeno@sonicbyte.com>
2013-12-02 11:01:48 +0100

    Add department/modality selection functionality

--- app/controllers/personal_controller.rb
+++ app/controllers/personal_controller.rb
@@ -1,9 +1,7 @@
 class PersonalController < ApplicationController
   def index
-    if params[:modality]
-      @people = Personal.where(modality_description: params[:modality])
-    else
-      @people = Personal.select(:age, :payroll, :triennia, :sex).to_a
-    end
+    @people = Personal.select(:age, :payroll, :triennia, :sex)
+    @people = @people.where(modality_description:   params[:modality])   if params[:modality].present?
+    @people = @people.where(department_description: params[:department]) if params[:department].present?
   end
 end
