2f51fc7fcf019908418244e51d5699ffbd7cd5bb
danilat <danilat83@gmail.com>
2014-01-22 02:22:36 +0100

    added order to agreements list

--- app/controllers/agreement_controller.rb
+++ app/controllers/agreement_controller.rb
@@ -14,10 +14,23 @@ class AgreementController < ApplicationController
     end
     year_ini = Date.new(year_ini, 1, 1)
     year_end = Date.new(year_end, 12, 31)
-    
+
+    case params[:type]
+    when "amount"
+      order_by = "total_amount"
+    when "entities"
+      order_by = "number_of_signatories"
+    when "percentage"
+      order_by = "dga_contribution_percentage"
+    else
+      order_by = "id"
+    end
     title = params[:title]? "%#{params[:title]}%": "%"
     signatories = params[:signatories]? "%#{params[:signatories]}%": "%"
-    @agreements = Agreement.where(:agreement_date => year_ini..year_end).where("lower(title) like lower(?)", title).where("lower(signatories) like lower(?)", signatories)
+    @agreements = Agreement.where(:agreement_date => year_ini..year_end)
+      .where("lower(title) like lower(?)", title)
+      .where("lower(signatories) like lower(?)", signatories)
+      .order("#{order_by} DESC")
     @paginated_agreements = @agreements.paginate(:page => params[:page], :per_page => 5)
   end
 
