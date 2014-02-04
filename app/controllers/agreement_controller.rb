2042a27f3d349d9250a54421f9200e1ba3703a80
danilat <danilat83@gmail.com>
2014-02-04 21:26:30 +0100

    moved all the model logic (really queries) from controller to model scopes

--- app/controllers/agreement_controller.rb
+++ app/controllers/agreement_controller.rb
@@ -1,31 +1,11 @@
 class AgreementController < ApplicationController
   def index
-    
-    year_ini = params[:year_ini].to_i
-    year_end = params[:year_end].to_i
-    year_ini = 2008 if (year_ini < 2008)
-    year_end = 2013 if (year_end < 2008)
-
-    
-    year_ini = Date.new(year_ini, 1, 1)
-    year_end = Date.new(year_end, 12, 31)
-
-    case params[:type]
-    when "amount"
-      order_by = "total_amount"
-    when "entities"
-      order_by = "number_of_signatories"
-    when "percentage"
-      order_by = "dga_contribution_percentage"
-    else
-      order_by = "id"
-    end
-    title = params[:title]? "%#{params[:title]}%": "%"
-    signatories = params[:signatories]? "%#{params[:signatories]}%": "%"
-    @agreements = Agreement.where(:agreement_date => year_ini..year_end)
-      .where("lower(title) like lower(?)", title)
-      .where("lower(signatories) like lower(?)", signatories)
-      .order("#{order_by} DESC")
+    @agreements = Agreement.between_dates(params[:year_ini].to_i,params[:year_end].to_i)
+      .valid(params[:validity_date])
+      .invalid(params[:validity_date])
+      .find_by_title(params[:title])
+      .find_by_signatories(params[:signatories])
+      .order_by(params[:type])
     @paginated_agreements = @agreements.paginate(:page => params[:page], :per_page => 5)
   end
 
--- app/models/agreement.rb
+++ app/models/agreement.rb
@@ -1,8 +1,41 @@
 class Agreement < ActiveRecord::Base
+  VALID = "0"
+  INVALID = "1"
   attr_accessible :code, :year, :section, :title, :agreement_date, :signature_date,
     :validity_date, :signatories, :number_of_signatories, :dga_contribution, :another_contributions, :amount,
     :addendums, :observations, :notes, :pdf_url, :total_amount, :total_dga_contribution,
     :dga_contribution_percentage
+
+  scope :valid, ->(val) { where("validity_date > ?", DateTime.now.to_date) if val == VALID}
+  scope :invalid, ->(val) { where("validity_date < ?", DateTime.now.to_date) if val == INVALID}
+  scope :between_dates, -> (year_ini, year_end) {
+    year_ini = 2008 if (year_ini < 2008)
+    year_end = 2013 if (year_end < 2008)
+    year_ini = Date.new(year_ini, 1, 1)
+    year_end = Date.new(year_end, 12, 31)
+    where(:agreement_date => year_ini..year_end)
+  }
+  scope :find_by_title, -> (title) {
+    title = title ? "%#{title}%": "%"
+    where("lower(title) like lower(?)", title)
+  }
+  scope :find_by_signatories , -> (signatories) {
+    signatories = signatories ? "%#{signatories}%": "%"
+    where("lower(signatories) like lower(?)", signatories)
+  }
+  scope :order_by, -> (type) {
+    case type
+    when "amount"
+      order_by = "total_amount"
+    when "entities"
+      order_by = "number_of_signatories"
+    when "percentage"
+      order_by = "dga_contribution_percentage"
+    else
+      order_by = "id"
+    end
+    order("#{order_by} DESC")
+  }
 
   def pdf_urls
     urls = []
