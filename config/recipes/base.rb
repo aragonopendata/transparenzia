f036dde63310fa5d118b3b9060935122ed10d180
Pablo Jimeno <pdjimeno@gmail.com>
2014-06-11 08:23:07 +0200

    Extract ubuntu upgrade

--- config/recipes/base.rb
+++ config/recipes/base.rb
@@ -10,9 +10,6 @@ end
 namespace :deploy do
 	desc "Install everything onto the server"
 	task :install do
-		run "#{sudo} apt-get -y update"
-		run "#{sudo} apt-get install update-manager-core"
-		run "#{sudo} do-release-upgrade -d"
 		run "#{sudo} apt-get -y update"		
 		run "#{sudo} apt-get -y install python-software-properties"
 	end
